ffd01146e3d1e4bb74d487ede00aa0db6347b019
Itamar Almeida de Oliveira <ialmeida@puc-rio.br>
2020-11-02 05:15:27 -0300

    add teste do modulo configure_data_app

--- tests/tests_data_tital.py
+++ tests/tests_data_tital.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import os
 from io import TextIOWrapper
 import unittest
@@ -17,7 +18,7 @@ class TestDataTital(unittest.TestCase):
         self.assertNotIsInstance(
             type(self.tidal.arqv),
             TextIOWrapper,
-            "Nao eh um tipo valido de entrada de texto!")
+            "Não é um tipo válido de entrada de texto!")
 
     def test_read_data(self):
         self.tidal.get_data_from_file()
@@ -26,27 +27,27 @@ class TestDataTital(unittest.TestCase):
         self.assertIsInstance(
             self.tidal.get_data_from_file(),
             list,
-            "nao eh uma lista!"
+            "não é uma lista!"
         )
 
     def test_exist_metadata(self):
         self.assertEqual(
             hasattr(self.tidal, 'get_metadata'),
             True,
-            "Nao existe atributo metadata!")
+            "Não existe atributo metadata!")
 
     def test_metadata_has_lines(self):
         self.assertEqual(
             len(self.tidal.get_metadata()) > 0,
             True,
-            "Nao existe linhas"
+            "Não existe linhas!"
         )
 
     def test_remove_broken_line_exist(self):
         self.assertEqual(
             hasattr(self.tidal, "remove_broken_line"),
             True,
-            "Nao existe dados possuem quebra de linha!"
+            "Dados possuem quebra de linha!"
         )
 
     def test_remove_broken_line_is_list(self):
@@ -60,11 +61,18 @@ class TestDataTital(unittest.TestCase):
         self.assertEqual(
             hasattr(self.tidal, "process_line"),
             True,
-            "Nao existe Process Line!"
+            "Não existe func process_line!"
         )
 
     def test_format_date(self):
-        self.assertEqual(hasattr(self.tidal, 'format_date'), True, "nao possui format date")
+        self.assertEqual(
+            hasattr(
+                self.tidal,
+                'format_date'
+            ),
+            True,
+            "não possui format date"
+        )
         for line in self.tidal.get_data_from_file():
             self.assertIsInstance(line[0], datetime, line[0])
 
@@ -74,14 +82,14 @@ class TestDataTital(unittest.TestCase):
                 "\n" in line,
                 False,
                 "linha sem tratamento"
-        )
+            )
 
         for line in self.tidal.get_metadata():
             self.assertEqual(
                 "\n" in line,
                 False,
                 "linha sem tratamento"
-        )
+            )
 
     def test_metadata_is_dict(self):
         self.assertIsInstance(
@@ -99,3 +107,40 @@ class TestDataTital(unittest.TestCase):
 
     def test_get_station_name(self):
         pass
+
+    def test_data_from_file_is_dict(self):
+        self.assertEqual(
+            type(self.tidal.data2dict()),
+            dict,
+            "não é um dicionario!"
+        )
+
+    def test_data2dict(self):
+        self.assertEqual(
+            hasattr(
+                self.tidal,
+                'data2dict'
+            ),
+            True,
+            "Não existe data2dict!!"
+        )
+
+    def test_water_level_is_float(self):
+        for k, v in self.tidal.data2dict().items():
+            self.assertIsInstance(v, float, "data type has must be a float!")
+
+    def test_water_level_is_digit(self):
+        for line in self.tidal.get_data_from_file():
+            self.assertEqual(
+                line[1].isdigit(),
+                False,
+                "date dont is digit!"
+            )
+
+    def test_tzinfo_from_date(self):
+        for k, v in self.tidal.data2dict().items():
+            self.assertEqual(k.tzinfo != None, True, 'Time zona não definido!!')
+
+    def test_metadata_value_is_list(self):
+        for k, v in self.tidal.get_metadata().items():
+            self.assertIsInstance(v, list, "valores do metadata nao eh uma lista")
