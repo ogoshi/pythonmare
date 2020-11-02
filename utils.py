18c59ca69383b19b6e0b87679aac358c2602abd3
Itamar Almeida de Oliveira <ialmeida@puc-rio.br>
2020-11-02 07:18:58 -0300

    add uma serie de utilitarios como items do menu e das abas do app

---
+++ utils.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+this_path = os.path.realpath('__file__')
+this_dir = os.path.dirname(this_path)
+
+menu_items = ["File", "Estilo"]
+
+
+items_notebook = ["Pythonmare", "Resultados"]
