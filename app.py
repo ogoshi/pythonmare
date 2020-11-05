c15ac9a672b495c7576ca2be8d10fdf585608f5f
Itamar Almeida de Oliveira <ialmeida@puc-rio.br>
2020-11-05 06:35:16 -0300

    alterado definicao dos layout definido metodos de geometria do layout frames herdam panedWindow

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ from utils import (
 from tree_viewer import TreeViewer
 from app_plot import TidalGraph, changeExchangeData
 from tidal_data import TidalData
-
 from tkinter import filedialog
+plugin = None
 
 
 LARGE_FONT = ("Verdana", 12)
@@ -27,39 +27,62 @@ def popupmsg(msg):
     popup.mainloop()
 
 
-class PythonMareApplication(tk.Tk):
-    def __init__(self):
-        super().__init__()
-        self.title('Python Mare DataViewer')
+class PythonMareApplication(tk.Frame):
+    def __init__(self, parent=None, data=None):
+        self.parent = parent
+        if not self.parent:
+            tk.Frame.__init__(self)
+            self.main = self.master
+        else:
+            self.main = tk.Toplevel()
+            self.master = self.main
         self.frames = {}
-        self.new_data = {}
-        self.notebooks = self.make_notebook(self)
-        self.menubar = tk.Menu(self)
-        self.container = tk.Frame(self.notebooks['Pythonmare'])
-        self.container.pack(side="right", fill="x", expand=True)
-        self.container.grid_rowconfigure(0, weight=1)
-        self.container.grid_columnconfigure(0, weight=1)
-
-        self.setup_frames(self.container)
-
+        self.main.title('Mare - visualizador de dados')
         self.setup_menu()
+        self.setup_gui()
+        self.setup_frames(self.nb, [TreeViewer, TidalGraph])
 
-    def setup_frames(self, parent):
-        cont = 0
+    def setup_gui(self):
+        self.m = tk.PanedWindow(self.main, orient=tk.VERTICAL)
+        self.m.pack(fill=tk.BOTH, expand=1)
+        self.nb = ttk.Notebook(self.main)
+        self.m.add(self.nb)
+        self.set_geometry()
+        return
 
-        for F in (TreeViewer, TidalGraph):
-            frame = F(parent, self)
+    def get_best_geometry(self):
+        ws = self.main.winfo_screenwidth()
+        hs = self.main.winfo_screenheight()
+        self.w = w = ws/1.4
+        h = hs*0.7
+        x = (ws/2)-(w/2)
+        y = (hs/2)-(h/2)
+        g = '%dx%d+%d+%d' % (w, h+10, x, y)
+        return g
+
+    def set_geometry(self):
+        self.winsize = self.get_best_geometry()
+        self.main.geometry(self.winsize)
+        return
+
+    def setup_frames(self, parent, frame_list):
+        main = tk.PanedWindow(orient=tk.HORIZONTAL)
+        self.nb.add(main, text='ExploreMare')
+
+        for F in frame_list:
+            f1 = tk.Frame(main)
+            frame = F(f1, self)
             self.frames[F] = frame
-            frame.pack(side=tk.LEFT, fill=tk.Y, expand=tk.TRUE, padx=3, pady=2)
-            cont += 1
+            frame.pack(side=tk.LEFT, fill=tk.Y, expand=tk.TRUE, padx=0, pady=0)
+            f1.pack(side=tk.LEFT, fill=tk.Y, expand=tk.TRUE, padx=0, pady=0)
 
     def setup_menu(self):
         menubar = tk.Menu(self)
 
         filemenu = tk.Menu(menubar, tearoff=0)
         filemenu.add_command(
-            label="Save settings",
-            command=lambda: popupmsg("Not supported just yet!")
+            label="Open",
+            command=lambda: self.open_data(self.frames[TreeViewer].action)
         )
         filemenu.add_separator()
         filemenu.add_command(label="Exit", command=quit)
@@ -89,8 +112,7 @@ class PythonMareApplication(tk.Tk):
         )
 
         menubar.add_cascade(label="Definir Estilo", menu=style)
-
-        tk.Tk.config(self, menu=menubar)
+        self.main.config(menu=menubar)
 
     def open_data(self, event):
         filename = filedialog.askopenfilename()
@@ -104,17 +126,10 @@ class PythonMareApplication(tk.Tk):
         changeExchangeData(data, metadata)
 
     def make_notebook(self, parent):
-        abas = ttk.Notebook(parent)
         dict_notebook = {}
         for item in items_notebook:
-            dict_notebook[item] = tk.Frame(abas)
-            abas.add(dict_notebook[item], text=item)
-
-        abas.pack(
-            side=tk.RIGHT,
-            fill=tk.BOTH,
-            expand=tk.TRUE
-        )
+            dict_notebook[item] = tk.Frame(self.nb)
+            self.nb.add(dict_notebook[item], text=item)
         return dict_notebook
 
     def show_frame(self, conteudo):
