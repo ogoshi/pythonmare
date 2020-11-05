d18272e0ab90220208fbd52b28d9c66de5adb6bb
Itamar Almeida de Oliveira <ialmeida@puc-rio.br>
2020-11-05 06:36:20 -0300

    app frame alterado atributo controller para callback

--- app_frame.py
+++ app_frame.py
@@ -10,16 +10,16 @@ class AppFrame(tk.Frame):
         self.map_frames = {}
         self.tree = None
 
-    def add_button(self, name, controller):
-        frame = tk.Frame(self)
-
+    def add_button(self, name, callback):
+        main = self.main
+        frame = tk.Frame(main)
         button = ttk.Button(
             frame,
             text=name,
-            command=lambda: controller(self.action)
+            command=lambda: callback(self.action)
         )
         frame.pack(side=tk.TOP)
-        button.pack(side=tk.LEFT)
+        button.pack(side=tk.LEFT,  expand=1)
 
         self.add_frame2map(name, button)
 
